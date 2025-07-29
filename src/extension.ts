16c0ac57bd7db69f7ce02e61251ac7a1c8cb69f2
pxor <nedislav@gmail.com>
2025-07-29 17:03:26 +0300

    feat: Add custom selection from .local/share/codetracer files for trace folders

--- src/extension.ts
+++ src/extension.ts
@@ -2,25 +2,46 @@ import * as vscode from "vscode";
 import { ChildProcess } from "child_process";
 import { initPanels, disposePanels, disposeCommands } from "./initPanels";
 import * as utils from "./utils";
+import * as path from "path";
+import * as os from "os";
+import * as fs from "fs";
 import {
   MediatorWithSubscribers,
   DapVsCodeApi,
   setupVsCodeExtensionViewsApi,
   newDapVsCodeApi,
   setupEditorApi,
-  setupMiddlewareApis
+  setupMiddlewareApis,
+  completeMove,
+  vsUpdatedFlow
 } from "./ct_vscode.js";
 
 let backendProcess: ChildProcess | null = null;
-
 let ctStarted = false;
-
 let nextStepDisposable: vscode.Disposable | null = null;
 
 const TYPE_KIND_INT = 7;
 
 function intValue(i: number): any {
   return { kind: TYPE_KIND_INT, i: i.toString() };
+}
+
+async function pickTraceFolder(): Promise<string | undefined> {
+  const baseDir = path.join(os.homedir(), '.local', 'share', 'codetracer');
+  const folders = fs.readdirSync(baseDir).filter(name =>
+    fs.statSync(path.join(baseDir, name)).isDirectory()
+  ).reverse(); // Reverse the order (latest last => first)
+
+  const picked = await vscode.window.showQuickPick(folders, {
+    placeHolder: 'Select a trace folder to use',
+    canPickMany: false
+  });
+
+  if (picked) {
+    return path.join(baseDir, picked);
+  }
+
+  return undefined;
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -50,10 +71,17 @@ export function activate(context: vscode.ExtensionContext) {
       } else {
         // Start CT
         ctStarted = true;
-        // const callerPid = process.pid.toString();
+
         const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
         if (!workspaceFolder) {
           vscode.window.showErrorMessage("No workspace folder is open.");
+          return;
+        }
+
+        // Trace selector
+        const selectedFile = await pickTraceFolder()
+        if (!selectedFile) {
+          vscode.window.showWarningMessage('No trace folder selected.');
           return;
         }
 
@@ -62,19 +90,48 @@ export function activate(context: vscode.ExtensionContext) {
         );
         (vscode.window as any).viewsApi = viewsApi; // easier debugging
         const dapVsCodeApi = newDapVsCodeApi(vscode, context);
-		
-        setupMiddlewareApis(dapVsCodeApi, viewsApi);
-        setupEditorApi(dapVsCodeApi, vscode, context, vscode.window.activeTextEditor);
+        (vscode.window as any).dapVsCodeApi = dapVsCodeApi;
 
+        // Setup middleware
+        // dapVsCodeApi.flowFunction = vsUpdatedFlow;
+        // dapVsCodeApi.completeMoveFunction = completeMove;
+        // completeMove(vscode.window.activeTextEditor, dapVsCodeApi);
+        // console.log(dapVsCodeApi);
+        setupMiddlewareApis(dapVsCodeApi, viewsApi);
+        // setupEditorApi(dapVsCodeApi, vscode, context, vscode.window.activeTextEditor);
+        
+        // Initialize panels
         const panels = initPanels(context, viewsApi);
         (vscode.window as any).panels = panels; // easier debugging
+
+        // // TODO: Find place for folder selection in the logic
+        // Get custom folder
+        // const codetracerFolder = vscode.Uri.joinPath(context.extensionUri, 'media', 'codetracer-data');
+
+        // const folderUri = await vscode.window.showOpenDialog({
+        //   canSelectFiles: false,
+        //   canSelectFolders: true,
+        //   canSelectMany: false,
+        //   openLabel: 'Select Codetracer Folder',
+        //   defaultUri: vscode.Uri.file(codetracerFolder.path)
+        // });
+
+        // // TODO: For now hardcode for easier development
+        // let selectedFile = "~/.local/share/codetracer/trace-3"
+
+        // if (folderUri && folderUri.length > 0) {
+        //   selectedFile = folderUri[0].fsPath;
+        //   vscode.window.showInformationMessage(`You selected: ${selectedFile}`);
+        // } else {
+        //   vscode.window.showWarningMessage('No file selected');
+        // }
 
         const debugConfig = {
           type: "codetracer-debug",
           request: "launch",
           name: "Launch Codetracer",
           cwd: "",
-          traceFolder: "~/.local/share/codetracer/trace-3",
+          traceFolder: selectedFile
         };
 
         const started = await vscode.debug.startDebugging(
